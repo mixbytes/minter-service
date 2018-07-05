c2a5a1ee94007f3690beda8a7df6078b1b16bbbb
Konstantin Privezentsev <privezentsev@gmail.com>
2018-07-05 18:10:31 +0300

    [DEV-146] remove ether balance method

--- contracts/IICOInfo.sol
+++ contracts/IICOInfo.sol
@@ -3,6 +3,5 @@ pragma solidity ^0.4.15;
 contract IICOInfo {
   function estimate(uint256 _wei) public constant returns (uint tokens);
   function purchasedTokenBalanceOf(address addr) public constant returns (uint256 tokens);
-  function sentEtherBalanceOf(address addr) public constant returns (uint256 _wei);
   function isSaleActive() public constant returns (bool active);
 }
